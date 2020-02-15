9431c61ebf3244d187b13e803d56f801ba7d8b3d
gungurbuz <gunguneypro@gmail.com>
2020-02-15 10:14:39 +0000

    added exponents

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
-sayi1 = int(input("sayi1 gir: "))
-sayi2 = int(input("sayi2 gir: "))
+
 #taking the numbers as an input
 
+
+
+
+
+def uslusayi ():
+  us = int(input("sayının üssünü gir: "))
+  print ("sonuç:")
+  print (sayi1 ** us)
 def toplama ():
- print ("___________________")
- print ("sonuç:")
- print (sayi1 + sayi2)
+  print ("sonuç:")
+  print (sayi1 + sayi2)
  #addition
 
 def carpma ():
-  print("___________________")
   print ("sonuç:")
   print (sayi1 * sayi2)
 	#multiplication
 
 def bolme ():
-  print("___________________")
   print ("sonuç:")
   print (sayi1 / sayi2)
 	#division
 
 def cikarma ():
-  print("___________________")
   print ("sonuç:")
   print (sayi1 - sayi2)
 	#subtraction
 
-
-
-print("___________________")
 print ("işlem seç")
 print ("1= toplama")
 print ("2= çıkarma")
 print ("3= çarpma")
 print ("4= bölme")
+print ("5= üslü sayı **sayı2 bu işlemde kullanılmayacaktır**")
 
 secim = input("işlemini seç: ")
 #asking the user to choose an operation
+
+sayi1 = int(input("sayı1 gir: "))
+sayi2 = int(input("sayı2 gir: "))
 
 if secim == "1":
   toplama()
@@ -46,5 +50,8 @@ elif secim == "3":
   carpma()
 elif secim == "4":
   bolme()
+elif secim == "5":
+  uslusayi()
+
 #the if elif function to execute when the 
 #representing number is chosen
