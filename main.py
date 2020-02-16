456d49c03d3db926d21740e08353eefe403bd03d
gungurbuz <gunguneypro@gmail.com>
2020-02-16 08:58:08 +0000

    made a processing delay for all operations and made the square root operation ask for only one number

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import time
 def uslusayi ():
   print ("sonuç:")
   print (sayi1 ** sayi2)
@@ -26,6 +27,7 @@ def cikarma ():
 def karekok ():
   print ("sonuç:")
   print (math.sqrt(sayi1))
+  #square root
 
 print ("işlem seç")
 print ("1= toplama")
@@ -38,21 +40,66 @@ print ("6= karekök ** sadece sayı1 kullanılacaktır **")
 secim = input("işlemini seç: ")
 #asking the user to choose an operation
 
-sayi1 = int(input("sayı1 gir: "))
-sayi2 = int(input("sayı2 gir: "))
-#taking the numbers as an input
+
+
 
 if secim == "1":
+  sayi1 = int(input("sayı1 gir: "))
+  sayi2 = int(input("sayı2 gir: "))
+  #taking the numbers as an input
+  print("hesaplanıyor")
+  time.sleep(3)
   toplama()
 elif secim == "2":
+  sayi1 = int(input("sayı1 gir: "))
+  sayi2 = int(input("sayı2 gir: "))
+  print("hesaplanıyor")
+  time.sleep(3)
   cikarma()
 elif secim == "3":
+  sayi1 = int(input("sayı1 gir: "))
+  sayi2 = int(input("sayı2 gir: "))
+  print("hesaplanıyor")
+  time.sleep(3)
   carpma()
 elif secim == "4":
+  sayi1 = int(input("sayı1 gir: "))
+  sayi2 = int(input("sayı2 gir: ")) 
+  print("hesaplanıyor")
+  time.sleep(3)
   bolme()
 elif secim == "5":
+  sayi1 = int(input("sayı1 gir: "))
+  sayi2 = int(input("sayı2 gir: "))
+  print("hesaplanıyor")
+  time.sleep(3)
   uslusayi()
 elif secim == "6":
+  sayi1 = int(input("sayı1 gir: "))
+  print("hesaplanıyor")
+  time.sleep(3)
   karekok()
 #the if elif function to execute when the 
 #representing number is chosen
+
+
+#print (".")
+#time.sleep(0.2)
+#print ("..")
+#time.sleep(0.2)
+#print ("...")
+#time.sleep(0.2)
+#print ("....")
+#time.sleep(0.2)
+#print (".....")
+#time.sleep(0.2)
+#print ("......")
+#time.sleep(0.2)
+#print (".......")
+#time.sleep(0.2)
+#print ("........")
+#time.sleep(0.2)
+#print (".........")
+#time.sleep(0.2)
+#print ("..........")
+#ill try to make this work later
