df142ba070ffe08ebf24f1cfd945f6539353c938
gungurbuz <gunguneypro@gmail.com>
2020-02-18 12:30:39 +0000

    added factorials and made it loop until asked otherwise

--- main.py
+++ main.py
@@ -1,87 +1,115 @@
 import math
 import time
-def uslusayi ():
+while True:
+ def faktoryel():
+  print ("sonuç:")
+  print (math.factorial(math.fabs(sayi1)))
+
+ def uslusayi ():
   print ("sonuç:")
   print (sayi1 ** sayi2)
 
-def toplama ():
+ def toplama ():
   print ("sonuç:")
   print (sayi1 + sayi2)
  #addition
 
-def carpma ():
+ def carpma ():
   print ("sonuç:")
   print (sayi1 * sayi2)
 	#multiplication
 
-def bolme ():
+ def bolme ():
   print ("sonuç:")
   print (sayi1 / sayi2)
 	#division
 
-def cikarma ():
+ def cikarma ():
   print ("sonuç:")
   print (sayi1 - sayi2)
 	#subtraction
 
-def karekok ():
+ def karekok ():
   print ("sonuç:")
   print (math.sqrt(sayi1))
   #square root
 
-print ("işlem seç")
-print ("1= toplama")
-print ("2= çıkarma")
-print ("3= çarpma")
-print ("4= bölme")
-print ("5= üslü sayı ** sayı1 taban, sayı2 üs olacaktır **")
-print ("6= karekök ** sadece sayı1 kullanılacaktır **")
+ print ("işlem seç")
+ print ("1= toplama")
+ print ("2= çıkarma")
+ print ("3= çarpma")
+ print ("4= bölme")
+ print ("5= üslü sayı ** sayı1 taban, sayı2 üs olacaktır **")
+ print ("6= karekök ** sadece sayı1 kullanılacaktır **")
+ print ("7= faktöryel")
 
-secim = input("işlemini seç: ")
-#asking the user to choose an operation
+ secim = input("işlemini seç: ")
+ #asking the user to choose an operation
 
 
 
 
-if secim == "1":
+ if secim == "1":
   sayi1 = int(input("sayı1 gir: "))
   sayi2 = int(input("sayı2 gir: "))
   #taking the numbers as an input
   print("hesaplanıyor")
   time.sleep(3)
   toplama()
-elif secim == "2":
+  time.sleep(3.5)
+ elif secim == "2":
   sayi1 = int(input("sayı1 gir: "))
   sayi2 = int(input("sayı2 gir: "))
   print("hesaplanıyor")
   time.sleep(3)
   cikarma()
-elif secim == "3":
+  time.sleep(3.5)
+ elif secim == "3":
   sayi1 = int(input("sayı1 gir: "))
   sayi2 = int(input("sayı2 gir: "))
   print("hesaplanıyor")
   time.sleep(3)
   carpma()
-elif secim == "4":
+  time.sleep(3.5)
+ elif secim == "4":
   sayi1 = int(input("sayı1 gir: "))
   sayi2 = int(input("sayı2 gir: ")) 
   print("hesaplanıyor")
   time.sleep(3)
   bolme()
-elif secim == "5":
+  time.sleep(3.5)
+ elif secim == "5":
   sayi1 = int(input("sayı1 gir: "))
   sayi2 = int(input("sayı2 gir: "))
   print("hesaplanıyor")
   time.sleep(3)
   uslusayi()
-elif secim == "6":
+  time.sleep(3.5)
+ elif secim == "6":
   sayi1 = int(input("sayı1 gir: "))
   print("hesaplanıyor")
   time.sleep(3)
   karekok()
-#the if elif function to execute when the 
-#representing number is chosen
+  time.sleep(3.5)
+ elif secim == "7":
+  sayi1 = int(input("sayı1 gir: "))
+  print("hesaplanıyor")
+  time.sleep(3)
+  faktoryel()
+  time.sleep(3.5)
+  #the if elif function to execute when the 
+  #representing number is chosen
+ cevap = input("yeniden çalıştırmak istiyor musunuz?(E/H): ")
+ if cevap == "E" or cevap == "e":
+   print ("tamam ozaman")
 
+ elif cevap == "H" or cevap == "h":
+  print ("tamam ozaman baybay")
+  break
+
+ else:
+  print ("malsın sen e veya h yaz")
+  break
 
 #print (".")
 #time.sleep(0.2)
